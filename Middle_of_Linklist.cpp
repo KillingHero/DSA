91add510ce14190abf08d0ebf010e1aa7ee39798
KillingHero <wbasim9@gmail.com>
2026-09-26 10:13:54 +0500

    Solved middle of LinkedList

---
+++ Middle_of_Linklist.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        int size = 0;
+        ListNode *current = head;
+        while (current != nullptr)
+        {
+            size++;
+            current = current->next;
+        }
+
+        current = head;
+        for (int i = 0; i < size / 2; i++)
+        {
+            current = current->next;
+        }
+        return current;
+    }
+};
